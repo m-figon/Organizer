92bbbd21f97bfa11226a452f0929d27dd18c053d
mateusz <dalvan234@gmail.com>
2020-08-31 10:27:36 +0200

    hour adding and deleting whole item in list functionalities added

--- components/List.js
+++ components/List.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity, Alert } from 'react-native';
-
+import { AntDesign } from '@expo/vector-icons';
 
 export default class List extends Component {
     constructor(props) {
@@ -9,6 +9,7 @@ export default class List extends Component {
             users: null,
             user: null,
             newItem: "",
+            itemHour: "",
             logedAc: "SlickJoe"
         }
     }
@@ -34,8 +35,38 @@ export default class List extends Component {
             [type]: e.target.value
         })
     }
+    deleteItem = (item) =>{
+        console.log(item);
+        let userData = this.state.user;
+        userData.list.splice(item,1);
+        fetch('https://rocky-citadel-32862.herokuapp.com/Organizer/users/' + this.state.user.id, {
+                            method: 'PUT',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({
+                                email: this.state.user.email,
+                                account: this.state.user.account,
+                                password: this.state.user.password,
+                                list: userData.list,
+                                id: this.state.user.id
+                            }),
+                        }).then(() => {
+                            fetch('https://rocky-citadel-32862.herokuapp.com/Organizer/users')
+                                .then((response) => response.json())
+                                .then((responseJson) => {
+                                    this.setState({
+                                        users: responseJson,
+                                        newItem: "",
+                                        itemHour: ""
+                                    })
+                                })
+                        })
+    }
     addItem = () => {
-        if (this.state.user) {
+        console.log(!(this.state.newItem.match(/^[a-zA-Z]{4,15}$/) === null));
+        console.log(!(this.state.itemHour.match(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/) === null));
+        if (!(this.state.newItem.match(/^[a-zA-Z]{4,15}$/) === null) && !(this.state.itemHour.match(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/) === null)) {
             let user;
             let updatedList;
             for (let item of this.state.users) {
@@ -44,7 +75,10 @@ export default class List extends Component {
                         user: item
                     }, () => {
                         updatedList = item.list;
-                        updatedList.push(this.state.newItem);
+                        updatedList.push({
+                            title: this.state.newItem,
+                            hour: this.state.itemHour
+                        });
                         fetch('https://rocky-citadel-32862.herokuapp.com/Organizer/users/' + this.state.user.id, {
                             method: 'PUT',
                             headers: {
@@ -63,7 +97,8 @@ export default class List extends Component {
                                 .then((responseJson) => {
                                     this.setState({
                                         users: responseJson,
-                                        newItem: ""
+                                        newItem: "",
+                                        itemHour: ""
                                     })
                                 })
                         })
@@ -73,6 +108,12 @@ export default class List extends Component {
                 }
             }
 
+        }else if ((this.state.newItem.match(/^[a-zA-Z]{4,15}$/) === null) && !(this.state.itemHour.match(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/) === null)) {
+            alert('item value must consist of letters and be 4-15 letters long');
+        }else if (!(this.state.newItem.match(/^[a-zA-Z]{4,15}$/) === null) && (this.state.itemHour.match(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/) === null)) {
+            alert('time must be in HH:MM format');
+        }else{
+            alert('item value must consist of letters and be 4-15 letters long and time must be in HH:MM format');
         }
     }
     render() {
@@ -80,17 +121,27 @@ export default class List extends Component {
             return (
                 <View style={styles.list}>
                     <View style={styles.listContent}>
-                        <TextInput style={styles.inputContent} onChange={(e) => this.changeInput(e, 'newItem')} value={this.state.newItem} placeholder="Add new item"></TextInput>
+                        <View style={styles.line}>
+                            <TextInput style={styles.inputContent1} onChange={(e) => this.changeInput(e, 'newItem')} value={this.state.newItem} placeholder="Add new item"></TextInput>
+                            <TextInput style={styles.inputContent2} onChange={(e) => this.changeInput(e, 'itemHour')} value={this.state.itemHour} placeholder="00:00"></TextInput>
+                        </View>
                         <TouchableOpacity onPress={() => this.addItem()} style={styles.addbutton}>
                             <Text style={styles.addbutton}>Add</Text>
                         </TouchableOpacity>
                         <View style={styles.toDoItems}>
-                        {this.state.user.list.map((item) => {
-                            return (<Text style={styles.toDoItem}>{item}</Text>)
-                        })}
+                            {this.state.user.list.map((item,index) => {
+                                return (
+                                    <View style={styles.line}>
+                                        <Text style={styles.toDoItem1}>{item.title}</Text>
+                                        <Text style={styles.toDoItem2}>{item.hour}</Text>
+                                        <TouchableOpacity onPress={()=>{this.deleteItem(index)}}>
+                                            <AntDesign name="delete" size={24} color="black" />
+                                        </TouchableOpacity>
+                                    </View>)
+                            })}
                         </View>
                     </View>
-                    
+
                 </View>
             );
         } else {
@@ -107,8 +158,13 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         alignItems: 'center'
     },
+    line: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
     listContent: {
-        width: 250,
+        width: 300,
         backgroundColor: 'white',
         borderWidth: 1,
         paddingHorizontal: 30,
@@ -119,9 +175,17 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'flex-start'
     },
-    inputContent: {
+    inputContent1: {
         marginVertical: 20,
-        width: '100%',
+        width: '75%',
+        marginRight: '5%',
+        padding: 5,
+        borderBottomWidth: 1,
+        borderBottomColor: '#04d387'
+    },
+    inputContent2: {
+        marginVertical: 20,
+        width: '20%',
         padding: 5,
         borderBottomWidth: 1,
         borderBottomColor: '#04d387'
@@ -136,9 +200,19 @@ const styles = StyleSheet.create({
         color: 'white',
         borderRadius: 2
     },
-    toDoItem:{
+    toDoItem1: {
         marginVertical: 5,
-        width: '100%',
+        width: '75%',
+        marginRight: '2.5%',
+        padding: 5,
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 4
+    },
+    toDoItem2: {
+        marginVertical: 5,
+        width: '20%',
+        marginRight: '2.5%',
         padding: 5,
         borderWidth: 1,
         borderColor: 'gray',
